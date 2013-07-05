1cda0af5f71399340736c5befc39b075f78f0d17
Matt Przybylski <matt@reintroducing.com>
2013-07-04 23:07:29 -0500

    updated template description

--- template.js
+++ template.js
@@ -3,15 +3,16 @@
  * https://github.com/reintroducing/threshold
  *
  * Copyright (c) 2013 Matt Przybylski <matt@reintroducing.com>
+ * Licensed under the MIT license.
  */
 
 'use strict';
 
 // Basic template description.
-exports.description = 'Start a new project and hit the ground running.';
+exports.description = 'Start a new front end dev project with a Grunt workflow.';
 
 // Template-specific notes to be displayed before question prompts.
-exports.notes = 'Edit the Gruntfile.js file before grunt and run npm install.';
+exports.notes = 'Edit the Gruntfile.js file before running grunt and run npm install.';
 
 // Any existing file or directory matching this wildcard will cause a warning.
 exports.warnOn = 'Gruntfile.js';
